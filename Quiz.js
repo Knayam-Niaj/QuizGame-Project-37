13343291751d3b2b2d79477cb3a07db07c4da1fe
Knayam-Niaj <73075278+Knayam-Niaj@users.noreply.github.com>
2021-04-17 11:43:05 -0500

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    question.hide();
+
+    background("yellow");
+
+    var heading = createElement("h2");
+    heading.html("Quiz Results");
+    heading.position(300, 0);
+
+    Contestant.getPlayerInfo();
+
+
+    if(allContestants !== undefined){
+      fill("blue");
+      textSize(20);
+      text("NOTE: Contestant(s) who answered correctly are highlighted in green!", 130, 230);
+    }
+
+   for(var plr in allContestants){
+     var correctAns = "2";
+     if(correctAns === allContestants[plr].answer){
+       fill("green");
+     } else {
+       fill("red");
+     }
+   }
+    
+  }
+
+}
